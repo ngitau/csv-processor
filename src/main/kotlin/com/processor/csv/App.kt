a9aa8680e25062635fec3672aaf771492b810891
piedcrow <me@ndungugitau.com>
2019-12-08 13:29:18 +0300

    Initial set up

---
+++ src/main/kotlin/com/processor/csv/App.kt
@@ -0,0 +1,12 @@
+package com.processor.csv
+
+class App {
+  fun start(args: Array<String>) {
+    println("Yay!")
+  }
+}
+
+fun main(args: Array<String>) {
+  val app = App();
+  app.start(args)
+}
