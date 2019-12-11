4443f7ce6e8ef6b3ab87d28a2aed654133836845
Joseph G <joseph.gitau@radioafricagroup.co.ke>
2019-12-11 05:18:38 +0300

    Feature/reader (#1)
    * ~
    
    * Reads any CSV and renders in a table.
    
    * Renders specific CSV in set format, wholesome improvements
    
    * Cleans up classes

---
+++ src/main/java/com/processor/csv/util/CSVUtils.java
@@ -0,0 +1,124 @@
+package com.processor.csv.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class CSVUtils provides an array of functions for CSV operations
+ *
+ * created by Joseph G.
+ */
+
+public class CSVUtils {
+  private static final char DELIMITER = ',';
+  private static final char QUOTE = '"';
+
+  /**
+   *
+   * @param line  String of characters from CSV line
+   * @return List of strings
+   */
+  public static List<String> parseLine(String line) {
+    return parseLine(line, DELIMITER, QUOTE);
+  }
+
+  /**
+   *
+   * @param line String of characters from CSV line
+   * @param delimiter Character that marks end of a CSV column
+   * @param customQuote Quote characters to be escaped in column
+   * @return List of strings
+   */
+  private static List<String> parseLine(String line, char delimiter, char customQuote) {
+
+    List<String> result = new ArrayList<>();
+
+    // if line is empty, return empty list!
+    if (line == null && line.isEmpty()) {
+      return result;
+    }
+
+    // Defines quote character to escape
+    if (customQuote == ' ') {
+      customQuote = QUOTE;
+    }
+
+    // Defaults to the comma as the CSV delimiter
+    if (delimiter == ' ') {
+      delimiter = DELIMITER;
+    }
+
+    // Creates a string buffer to allow method calls on string
+    StringBuffer stringBuffer = new StringBuffer();
+
+    // Flags
+    boolean inQuotes = false;
+    boolean startCollectChar = false;
+    boolean doubleQuotesInColumn = false;
+
+    // Create character array from the line
+    char[] chars = line.toCharArray();
+
+    // Loop through every character
+    for (char ch : chars) {
+
+      if (inQuotes) {
+        startCollectChar = true;
+        if (ch == customQuote) {
+          inQuotes = false;
+          doubleQuotesInColumn = false;
+        } else {
+
+          // Allows empty quotes in custom quote enclosed
+          if (ch == '\"') {
+            if (!doubleQuotesInColumn) {
+              stringBuffer.append(ch);
+              doubleQuotesInColumn = true;
+            }
+          } else {
+            stringBuffer.append(ch);
+          }
+
+        }
+      } else {
+        if (ch == customQuote) {
+
+          inQuotes = true;
+
+          // Allows empty quotes empty quote enclosed
+          if (chars[0] != '"' && customQuote == '\"') {
+            stringBuffer.append('"');
+          }
+
+          // If double quotes present
+          if (startCollectChar) {
+            stringBuffer.append('"');
+          }
+
+        } else if (ch == delimiter) {
+
+          result.add(stringBuffer.toString());
+
+          stringBuffer = new StringBuffer();
+          startCollectChar = false;
+
+        } else if (ch == '\r') {
+          //ignore LF characters
+          continue;
+        } else if (ch == '\n') {
+          //the end, break!
+          break;
+        } else {
+          stringBuffer.append(ch);
+        }
+      }
+
+    }
+
+    // Add string buffer to result array
+    result.add(stringBuffer.toString());
+
+    return result;
+  }
+
+}
--- src/main/kotlin/com/processor/csv/App.kt
+++ src/main/kotlin/com/processor/csv/App.kt
@@ -1,29 +1,64 @@
 package com.processor.csv
 
-import com.processor.csv.helpers.CsvHelper
+import com.processor.csv.util.CSVHelper
 import org.jooby.Jooby.run
 import org.jooby.Kooby
 import org.jooby.Results
 import org.jooby.hbs.Hbs
+import java.io.File
+import java.net.URL
 
+/**
+ * Power up the web framework
+ *
+ * created by Joseph G.
+ */
 
 class App: Kooby({
+
+  // Handlebars Module for templating
   use(Hbs())
 
-  val reader = CsvHelper()
+  // Instantiates our CSV helper
+  val csvHelper = CSVHelper()
 
+  /**
+   * Receives HTTP.GET requests from '/' route
+   *
+   * @return Home Page
+   */
   get("/") {
     Results.html("home")
   }
 
-  post("/process") {
+  /**
+   * Receives HTTP.GET requests from '/smart-reader' route
+   *
+   * @return Smart Reader Page
+   */
+  get("/smart-reader") {
+    val fileName = "music-releases.csv"
+    val classLoader = javaClass.classLoader
+    val resource: URL? = classLoader.getResource(fileName)
+    if (resource == null) {
+      throw IllegalArgumentException("File $fileName could not found!")
+    } else {
+      val records = csvHelper.csvMapper(File(resource.file))
+      Results.html("smart").put(mapOf("records" to records, "title" to fileName))
+    }
+  }
 
+  /**
+   * Receives HTTP.POST requests from '/general-reader' route
+   *
+   * Receives multipart/form-data
+   *
+   * @return General Reader Page
+   */
+  post("/general-reader") {
     val upload = file("file").file()
-    val separator = param("separator").value()
-
-//    val records = reader.read(upload, separator)
-
-    Results.html("review")
+    val records = csvHelper.csvReader(upload)
+    Results.html("general").put(mapOf("records" to records, "title" to upload.name))
   }
 
 })
